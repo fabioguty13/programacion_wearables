a6ff6d1f7aed5c92a24e17a9f80292866c14e479
fabioguty13 <104875774+fabioguty13@users.noreply.github.com>
2026-03-20 14:03:06 -0500

    primer commit del proyecto

---
+++ src/01_variables.kt
@@ -0,0 +1,39 @@
+fun main(){
+
+    val x: Int = 5
+
+    var y: Int = 5
+
+    y += 1
+
+
+    //Variables Enteras
+    val one = 1
+    val edad:Int = 48
+    println("one $one")
+    println("edad $edad")
+
+    val threeBillion = 3000000000 // long
+    println("threeBillion $threeBillion")
+    val oneLong = 1L
+    val oneByte: Byte = 1
+
+    //Variables DECIMALES
+    val pi = 3.14
+    println("pi $pi")
+
+    val oneDouble = 1.0
+    val e = 2.7182818284
+    val eFloat = 2.7182818284f
+
+    //Caract y String
+    val aChat: Char = 'a'
+    val str = "Esto es una frase completa"
+    val str2:String = "Estos es una frase completa"
+
+    //Booleanos o logicos
+    val myTrue: Boolean = true
+    println("myTrue $myTrue")
+    val myFals: Boolean = false
+
+}
